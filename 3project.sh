4da6d3150a30cfb97af6f96b98da54fc63c8e6ef
ahmad alwa <31738076+ahmadalwa@users.noreply.github.com>
2022-10-30 17:41:55 +0200

    Create 3project.sh

---
+++ 3project.sh
@@ -0,0 +1,6 @@
+Bookmarks Keeper
+https://www.crio.do/projects/bash-bookmarks-keeper/
+Scraping Twitter Bookmarks
+https://www.crio.do/projects/bash-scraping-twitter-bookmarks/
+Machine Learning Preprocessing CLI
+https://www.crio.do/projects/python-preprocessor-cli/
